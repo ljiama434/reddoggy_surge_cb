52b23d62faa4382a2d5313d076cb46789d458edb
ljiama434 <ljiama434@gmail.com>
2020-10-20 16:04:01 +0800

    remove video

--- cb/lib/script.js
+++ cb/lib/script.js
@@ -5,35 +5,35 @@ $(function(){
 		area=['80%','360px']
 }
 
-window.layer.open({
-            type: 2,
-            title: false,
-            area: area,
-            shade: 0.8,
-            shadeClose: true,
-            content:'images/video.mp4',
-            btn:['Click Here!! ลงทะเบียนเพื่อส่ง 300THB'],
-            yes: function(index, layero){
-                window.location="https://lihi1.com/OEHV6/";
-            }
-        });
-        return false;
-    });
-    $(".closeBtn").click(function(){
-        $("#videoPop").fadeOut()
-    });
+// window.layer.open({
+//             type: 2,
+//             title: false,
+//             area: area,
+//             shade: 0.8,
+//             shadeClose: true,
+//             content:'images/video.mp4',
+//             btn:['Click Here!! ลงทะเบียนเพื่อส่ง 300THB'],
+//             yes: function(index, layero){
+//                 window.location="https://lihi1.com/OEHV6/";
+//             }
+//         });
+//         return false;
+//     });
+//     $(".closeBtn").click(function(){
+//         $("#videoPop").fadeOut()
+//     });
     
-});
+// });
 
-//往上的按鈕
-$(function(){
-			var btn_top = $('.btn_top');
-            btn_top.click(function () {
-                $('body,html').animate({
-                    scrollTop: 0
-                }, 600);
-                return false;
-            });
+// //往上的按鈕
+// $(function(){
+// 			var btn_top = $('.btn_top');
+//             btn_top.click(function () {
+//                 $('body,html').animate({
+//                     scrollTop: 0
+//                 }, 600);
+//                 return false;
+//             });
 
 	
-});
+// });
